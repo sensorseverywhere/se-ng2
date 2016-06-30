0a43db40d46e04c7832a8ce0e9f651a2cb41bb4d
wade <wade_mansell@hotmail.com>
2016-06-30 15:27:04 +1000

    basic project setup

---
+++ src/components/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sensorseverywhere',
+    template: '<h1>Welcome to Sensors Everywhere.'
+})
+export class AppComponent {
+    
+}
